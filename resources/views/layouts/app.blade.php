e2f00b2057d0658719eff922aa6318d217eb2b90
Pauwlo <dev@pauwlo.fr>
2019-06-22 00:07:23 +0200

    Disable HTTP referrer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="referrer" content="no-referrer">
     <meta name="msapplication-TileColor" content="#2b5797">
     <meta name="theme-color" content="#ffffff">
     @if ($__env->yieldContent('description'))<meta name="description" content="@yield('description')">@endif
