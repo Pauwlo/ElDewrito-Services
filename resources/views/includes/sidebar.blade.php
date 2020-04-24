082453930d3d759bee7cce3de5c7373385c2a7d8
Pauwlo <dev@pauwlo.fr>
2020-04-24 21:30:24 +0200

    Sidebar improvements

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -34,7 +34,14 @@
         <ul class="navigation">
             <li{!! (Route::is('home')) ? ' class="navigation__active"' : '' !!}><a href="{{ route('home') }}"><i class="zwicon-home"></i> {{ __('Home') }}</a></li>
 
-            <li{!! (Route::is('official-playlists.*')) ? ' class="navigation__active"' : '' !!}><a href="{{ route('official-playlists.index') }}"><i class="zwicon-server-stack"></i> {{ __('Official Playlists') }}</a></li>
+            <li class="navigation__sub{!! (Route::is('official-playlists.*')) ? ' navigation__sub--active' : '' !!}">
+                <a href=""><i class="zwicon-server-stack"></i> {{ __('Official Playlists') }}</a>
+
+                <ul>
+                    <li{!! (Route::is('official-playlists.*') && !Route::is('official-playlists.maps.*')) ? ' class="navigation__active"' : '' !!}><a href="{{ route('official-playlists.index') }}">{{ __('Playlists') }}</a></li>
+                    <li{!! (Route::is('official-playlists.maps.*')) ? ' class="navigation__active"' : '' !!}><a href="{{ route('official-playlists.maps.index') }}">{{ __('Maps') }}</a></li>
+                </ul>
+            </li>
 
             <li class="navigation__sub">
                 <a href=""><i class="zwicon-web"></i> {{ __('External links') }}</a>
