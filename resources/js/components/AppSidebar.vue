473dd8aaa2d39085442e5793ef9e8151d7376966
Pauwlo <dev@pauwlo.fr>
2025-06-27 04:37:16 +0200

    Add link to this repo

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -5,7 +5,7 @@ import NavUser from '@/components/NavUser.vue';
 import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
 import { type NavItem } from '@/types';
 import { Link } from '@inertiajs/vue3';
-import { House, Server } from 'lucide-vue-next';
+import { Folder, House, Server } from 'lucide-vue-next';
 import AppLogo from './AppLogo.vue';
 
 const mainNavItems: NavItem[] = [
@@ -23,11 +23,11 @@ const mainNavItems: NavItem[] = [
 ];
 
 const footerNavItems: NavItem[] = [
-    /*{
-        title: 'Github Repo',
-        href: 'https://github.com/laravel/vue-starter-kit',
+    {
+        title: 'GitHub Repo',
+        href: 'https://github.com/Pauwlo/ElDewrito-Services',
         icon: Folder,
-    },*/
+    },
 ];
 </script>
 
