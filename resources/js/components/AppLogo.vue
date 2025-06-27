5da2c32a19af64c8ff19385386092859059e95d4
Pauwlo <dev@pauwlo.fr>
2025-06-27 03:32:15 +0200

    Add ElDewrito logo

--- resources/js/components/AppLogo.vue
+++ resources/js/components/AppLogo.vue
@@ -4,7 +4,7 @@ import AppLogoIcon from '@/components/AppLogoIcon.vue';
 
 <template>
     <div class="flex aspect-square size-8 items-center justify-center rounded-md bg-sidebar-primary text-sidebar-primary-foreground">
-        <AppLogoIcon class="size-5 fill-current text-white dark:text-black" />
+        <AppLogoIcon class="size-6" />
     </div>
     <div class="ml-1 grid flex-1 text-left text-sm">
         <span class="mb-0.5 truncate leading-tight font-semibold">ElDewrito Services</span>
