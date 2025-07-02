8783cc2df66d9335be6c7378b2091a9b88e76354
Pauwlo <dev@pauwlo.fr>
2025-07-02 04:11:37 +0200

    Add favicon

--- resources/views/plebbrowser.blade.php
+++ resources/views/plebbrowser.blade.php
@@ -24,9 +24,12 @@
 
         <title inertia>{{ config('app.name', 'ElDewrito Services') }}</title>
 
-        <link rel="icon" href="/favicon.ico" sizes="any">
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml">
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <meta name="apple-mobile-web-app-title" content="ED Services" />
+        <link rel="manifest" href="/site.webmanifest" />
 
         @routes
         @vite(['resources/js/app.ts', "resources/js/pages/{$page['component']}.vue"])
